98bd67009ba590a3445e35c9a161b22b325df9f0
D-Lovva <desireejcushnie@gmail.com>
2026-02-11 18:02:21 +0000

    add urls to product page

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
                <div className='text-3xl font-bold text-cream'>SodaPop</div>
                <ul className='flex space-x-6 text-cream'>
                  <li className='hover:text-mutedemerald cursor-pointer'>Home</li>
-                 <li className='hover:text-mutedemerald cursor-pointer'>Products</li>
+                 <li className='hover:text-mutedemerald cursor-pointer'><a href="/showproducts">Products</a></li>
                  <li className='hover:text-mutedemerald cursor-pointer'>About</li>
                  <li className='hover:text-mutedemerald cursor-pointer'>Contact</li>
                </ul>
